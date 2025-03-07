053b656c3c93e8112938784769ca24a234c21f86
Nyawakira Kevin <k.nyawakira@alustudent.com>
2025-03-07 01:44:57 -0800

    Pass

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -3,6 +3,7 @@
 is an instance of, or if the object is an instance of a class
 that inherited from, the specified class ; otherwise False."""
 
+
 def is_kind_of_class(obj, a_class):
     """check if the object is an instance of
     class and turns true otherwise false"""
